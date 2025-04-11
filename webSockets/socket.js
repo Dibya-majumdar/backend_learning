5cca63e9474c453009ce7b04c5c9876240fec0db
dibya <majumdardibya700@gmail.com>
2025-04-11 22:44:52 +0530

    change the frontend url according to host (if localhost then 3000 else ipv4 of aws server)

--- webSockets/socket.js
+++ webSockets/socket.js
@@ -5,7 +5,8 @@ const { chat } = require("../modules/socket");
 const initializeSocket=(server)=>{
     const io=socket(server,{           //we give http server inside socket so now in this http backend server websockets configured and we can control(send,receive data from frontend ..etc )
         cors:{                   //it is cors configuration that which frontend application can access this websocket
-            origin:"http://localhost:5173"
+            // origin:"http://localhost:5173"
+            origin:process.env.FRONTEND_ORIGIN || "http://localhost:5173"
         }
     });
     io.on("connection",(socket)=>{        //so io is like the boss but socket here represnt the user who connects to the server.
